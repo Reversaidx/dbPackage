3870a0e00f4e255dea2e5e17939ee08950f8374d
Dmitry F <dfedotov@tango.me>
2021-09-30 23:01:06 +0300

    init

---
+++ testdb.go
@@ -0,0 +1,86 @@
+package dbPackage
+
+import (
+	"errors"
+	"fmt"
+)
+
+type BackendDatabase interface {
+	New() error
+	Put([]byte, []byte) error
+	Get([]byte) ([]byte, error)
+	Delete([]byte) (error)
+	Close()
+	Stats() string
+	Flush() error
+}
+
+func (d *Database) New() error {
+	d.items=make(map[string][]byte)
+	return nil
+
+}
+var ErrKeyNotFound = errors.New("The key is not found")
+
+func (d *Database) Put(bytes []byte, bytes2 []byte) error {
+	_,ok:=d.items[string(bytes)]
+	if !ok{
+		d.items[string(bytes)]=bytes2
+		d.Stat.inserts++
+	}else {
+		d.items[string(bytes)]=bytes2
+		d.Stat.updates++
+	}
+	d.items[string(bytes)]=bytes2
+	return nil
+
+}
+
+func (d *Database) Get(bytes []byte) ([]byte, error) {
+	value,ok:=d.items[string(bytes)]
+	if !ok{
+		d.Stat.miss++
+		return nil, ErrKeyNotFound
+	}else {
+		d.Stat.hit++
+		return value,nil
+	}
+}
+
+func (d *Database) Delete(bytes []byte) error {
+	_,ok:=d.items[string(bytes)]
+	d.Stat.deletes++
+	if !ok{
+		return ErrKeyNotFound
+	}else {
+		delete(d.items,string(bytes))
+
+		return nil
+	}
+}
+
+func (d *Database) Close() {
+	d.items=nil
+}
+
+func (d *Database) Stats() string {
+	return fmt.Sprintf("inserts:%v\nupdates:%v\ndeletes:%v\nmiss:%v\nhit:%v\n", d.Stat.inserts,d.Stat.updates,d.Stat.deletes,d.Stat.miss,d.Stat.hit)
+}
+
+func (d *Database) Flush() error {
+	d.items=make(map[string][]byte)
+	return nil
+}
+
+type Database struct {
+	items    map[string][]byte
+	Stat
+}
+
+type Stat struct {
+	inserts int // no key
+	updates int // when key
+	deletes int
+	miss 	int // get to key
+	hit 	int // get failed
+}
---
+++ testdb_test.go
@@ -0,0 +1,52 @@
+package dbPackage
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDatabase(t *testing.T) {
+	t.Run("Complex", func(t *testing.T) {
+		var test Database
+		test.New()
+		test.Put([]byte("aaa"),[]byte("100"))
+		test.Put([]byte("bbb"),[]byte("200"))
+		test.Put([]byte("ccc"),[]byte("300"))
+		test.Put([]byte("ddd"),[]byte("400"))
+
+		//test put
+		val, ok :=test.Get([]byte("aaa"))
+		require.Nil(t, ok)
+		require.Equal(t, []byte("100"), val)
+		// test delete
+
+		test.Delete([]byte("aaa"))
+		val, ok =test.Get([]byte("aaa"))
+		// should be not found
+		require.NotNil(t, ok)
+
+		//update
+		test.Put([]byte("bbb"),[]byte("250"))
+		val, ok =test.Get([]byte("bbb"))
+		require.Nil(t, ok)
+		require.Equal(t, []byte("250"), val)
+
+		//flush
+		test.Flush()
+		val, ok =test.Get([]byte("bbb"))
+		// should be not found
+		require.NotNil(t, ok)
+
+
+		//testStat
+
+		stat:=test.Stats()
+
+		require.Equal(t, "inserts:4\nupdates:1\ndeletes:1\nmiss:2\nhit:2\n", stat)
+
+
+
+
+	})
+
+}
